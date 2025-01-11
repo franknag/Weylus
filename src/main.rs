1e25705e68040fca96d5606bcf6c8cf12af78c85
franknag <49418361+franknag@users.noreply.github.com>
2025-01-11 05:02:06 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ extern crate bitflags;
 extern crate objc;
 
 #[cfg(target_os = "macos")]
+extern crate macos_app_nap;
+
+#[cfg(target_os = "macos")]
 #[link(name = "thread_priority_helper")]
 extern "C" {
     fn setMaxPriority();
@@ -105,7 +108,7 @@ fn main() {
             }
         }
         // prevent macos app nap
-        crate::macos_app_nap::prevent();
+        macos_app_nap::prevent();
     }
 
     #[cfg(target_os = "linux")]
