f5cf3e5253feb39ab41a07a9ba90458211987e94
franknag <49418361+franknag@users.noreply.github.com>
2025-01-11 00:39:56 -0500

    default

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,3 @@
-#![allow(non_snake_case)]
-
-#[cfg(target_os = "macos")]
-#[macro_use]
-extern crate objc;
-
-use cocoa_foundation::base::nil;
-use cocoa_foundation::foundation::{NSProcessInfo, NSString};
-
-#[cfg(target_os = "macos")]
-#[link(name = "thread_priority_helper")]
-extern "C" {
-    fn setMaxPriority();
-}
-
-// Prevent display from sleeping/powering down, prevent system
-// from sleeping, prevent sudden termination for any reason.
-#[cfg(target_os = "macos")]
-pub fn prevent() {
-    let NSActivityIdleDisplaySleepDisabled = 1u64 << 40;
-    let NSActivityIdleSystemSleepDisabled = 1u64 << 20;
-    let NSActivitySuddenTerminationDisabled = 1u64 << 14;
-    let NSActivityAutomaticTerminationDisabled = 1u64 << 15;
-    let NSActivityUserInitiated = 0x00FFFFFFu64 | NSActivityIdleSystemSleepDisabled;
-    let NSActivityLatencyCritical = 0xFF00000000u64;
-
-    let options = NSActivityIdleDisplaySleepDisabled
-        | NSActivityIdleSystemSleepDisabled
-        | NSActivitySuddenTerminationDisabled
-        | NSActivityAutomaticTerminationDisabled;
-    let options = options | NSActivityUserInitiated | NSActivityLatencyCritical;
-
-    unsafe {
-        let pinfo = NSProcessInfo::processInfo(nil);
-        let s = NSString::alloc(nil).init_str("prevent app nap");
-        let _: () = msg_send![pinfo, beginActivityWithOptions:options reason:s];
-
-        setMaxPriority();
-    }
-}
-
 #![cfg_attr(feature = "bench", feature(test))]
 #[cfg(feature = "bench")]
 extern crate test;
@@ -102,12 +61,6 @@ fn main() {
     if conf.print_lib_js {
         print!("{}", web::LIB_JS);
         return;
-    }
-
-    #[cfg(target_os = "macos")]
-    {
-        // prevent app-nap to avoid connection issues
-        crate::macos_app_nap::prevent();
     }
 
     #[cfg(target_os = "linux")]
