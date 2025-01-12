f97ad61e7f412df65fa359781a5dd85f62ea2e91
franknag <49418361+franknag@users.noreply.github.com>
2025-01-12 09:01:57 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -77,32 +77,6 @@ fn main() {
         print!("{}", web::LIB_JS);
         return;
     }
-
-    #[cfg(target_os = "macos")]
-    {
-        // Prevent display from sleeping/powering down, prevent system
-        // from sleeping, prevent sudden termination for any reason.
-        pub fn prevent_nap() {
-            let NSActivityIdleSystemSleepDisabled = 1u64 << 20;
-            let NSActivitySuddenTerminationDisabled = 1u64 << 14;
-            let NSActivityAutomaticTerminationDisabled = 1u64 << 15;
-            let NSActivityUserInitiated = 0x00FFFFFFu64 | NSActivityIdleSystemSleepDisabled;
-            let NSActivityLatencyCritical = 0xFF00000000u64;
-
-            let options = NSActivityIdleSystemSleepDisabled
-                | NSActivitySuddenTerminationDisabled
-                | NSActivityAutomaticTerminationDisabled;
-            let options = options | NSActivityUserInitiated | NSActivityLatencyCritical;
-
-            unsafe {
-                let pinfo = NSProcessInfo::processInfo(nil);
-                let s = NSString::alloc(nil).init_str("prevent app nap");
-                let _:() = msg_send![pinfo, beginActivityWithOptions:options reason:s];
-
-            setMaxPriority();
-            }
-        }
-    }
     
     #[cfg(target_os = "linux")]
     {
