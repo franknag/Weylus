ae351e69b777c72c0e5efd0314f61b95c545da30
franknag <49418361+franknag@users.noreply.github.com>
2025-01-13 15:30:00 -0500

    Update thread_priority_helper.c

--- lib/thread_priority_helper.c
+++ lib/thread_priority_helper.c
@@ -8,23 +8,30 @@
 // Original comment: Obtained from the Stage psychophysics suite for Matlab.
 
 void setMaxPriority() {
-    int mib[2];
-    int busFreq;
-    size_t len;
-    thread_time_constraint_policy_data_t policy;
-    kern_return_t result;
+    //int mib[2];
+    //int busFreq;
+    //size_t len;
+    //thread_time_constraint_policy_data_t policy;
+    //kern_return_t result;
 
-    mib[0] = CTL_HW;
-    mib[1] = HW_BUS_FREQ;
-    len = sizeof(busFreq);
-    sysctl(mib, 2, &busFreq, &len, NULL, 0);
+    //mib[0] = CTL_HW;
+    //mib[1] = HW_BUS_FREQ;
+    //len = sizeof(busFreq);
+    //sysctl(mib, 2, &busFreq, &len, NULL, 0);
 
-    policy.period = busFreq / 120;
-    policy.computation = policy.period * 0.9;
-    policy.constraint = policy.computation;
-    policy.preemptible = 1;
+    //policy.period = busFreq / 120;
+    //policy.computation = policy.period * 0.9;
+    //policy.constraint = policy.computation;
+    //policy.preemptible = 1;
 
-    result = thread_policy_set(mach_thread_self(), THREAD_TIME_CONSTRAINT_POLICY, (thread_policy_t)&policy, THREAD_TIME_CONSTRAINT_POLICY_COUNT);
+    //result = thread_policy_set(mach_thread_self(), THREAD_TIME_CONSTRAINT_POLICY, (thread_policy_t)&policy, THREAD_TIME_CONSTRAINT_POLICY_COUNT);
+    result = thread_policy_set(1);
+    if (result) {
+        printf("Failed to set max priority\n");
+    }
+}
+
+void setDefaultPriority() {result = thread_policy_set(0);
     if (result) {
         printf("Failed to set max priority\n");
     }
