a02663fdaad10b26284eff6e5ef7ad3f51390986
franknag <49418361+franknag@users.noreply.github.com>
2025-01-12 20:31:02 -0500

    Update weylus.rs

--- src/weylus.rs
+++ src/weylus.rs
@@ -1,5 +1,3 @@
-#![allow(non_snake_case)]
-
 #[link(name = "thread_priority_helper")]
 extern "C" {
     fn setMaxPriority();
@@ -111,13 +109,15 @@ impl Weylus {
         {
             #![allow(non_snake_case)]
             {
+            let NSActivityIdleDisplaySleepDisabled = 1u64 << 40
             let NSActivityIdleSystemSleepDisabled = 1u64 << 20;
             let NSActivitySuddenTerminationDisabled = 1u64 << 14;
             let NSActivityAutomaticTerminationDisabled = 1u64 << 15;
             let NSActivityUserInitiated = 0x00FFFFFFu64 | NSActivityIdleSystemSleepDisabled;
             let NSActivityLatencyCritical = 0xFF00000000u64;
 
-            let options = NSActivityIdleSystemSleepDisabled
+            let options = NSActivityIdleDisplaySleepDisabled
+                | NSActivityIdleSystemSleepDisabled
                 | NSActivitySuddenTerminationDisabled
                 | NSActivityAutomaticTerminationDisabled;
             let options = options | NSActivityUserInitiated | NSActivityLatencyCritical;
