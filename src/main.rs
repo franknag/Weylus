53985f2c369f1f6048dacf247b504cba6cffc38b
franknag <49418361+franknag@users.noreply.github.com>
2025-01-13 11:55:26 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-#![allow(non_snake_case)]
+#![cfg_attr(feature = "bench", feature(test))]
+#[cfg(feature = "bench")]
+extern crate test;
+
 #[link(name = "thread_priority_helper")]
 extern "C" {
     fn setMaxPriority();
 }
-use cocoa_foundation::base::{nil};
-use cocoa_foundation::foundation::{NSProcessInfo, NSString};
-
-#[cfg_attr(feature = "bench", feature(test))]
-#[cfg(feature = "bench")]
-extern crate test;
 
 #[macro_use]
 extern crate bitflags;
@@ -77,26 +74,7 @@ fn main() {
 
     #[cfg(target_os = "macos")]
     {
-        let NSActivityIdleDisplaySleepDisabled = 1u64 << 40;
-    let NSActivityIdleSystemSleepDisabled = 1u64 << 20;
-    let NSActivitySuddenTerminationDisabled = 1u64 << 14;
-    let NSActivityAutomaticTerminationDisabled = 1u64 << 15;
-    let NSActivityUserInitiated = 0x00FFFFFFu64 | NSActivityIdleSystemSleepDisabled;
-    let NSActivityLatencyCritical = 0xFF00000000u64;
-
-    let options = NSActivityIdleDisplaySleepDisabled
-        | NSActivityIdleSystemSleepDisabled
-        | NSActivitySuddenTerminationDisabled
-        | NSActivityAutomaticTerminationDisabled;
-    let options = options | NSActivityUserInitiated | NSActivityLatencyCritical;
-
-    unsafe {
-        let pinfo = NSProcessInfo::processInfo(nil);
-        let s = NSString::alloc(nil).init_str("prevent app nap");
-        let _:() = msg_send![pinfo, beginActivityWithOptions:options reason:s];
-
         setMaxPriority();
-    }
     }
     #[cfg(target_os = "linux")]
     {
