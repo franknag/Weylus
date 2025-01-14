87866a1086412db6a7edeeb1c5a664ea19d48e18
franknag <49418361+franknag@users.noreply.github.com>
2025-01-14 00:16:16 -0500

    Update encode_video.c

--- lib/encode_video.c
+++ lib/encode_video.c
@@ -700,7 +700,7 @@ void open_video(VideoContext* ctx, Error* err)
 					ctx->c->color_range = AVCOL_RANGE_MPEG;
 					av_opt_set(ctx->c->priv_data, "realtime", "true", 0);
 					av_opt_set(ctx->c->priv_data, "allow_sw", "true", 0);
-					av_opt_set(ctx->c->priv_data, "bitrate", "40M", 0);
+					av_opt_set(ctx->c->priv_data, "profile", "baseline", 0);
 					set_codec_params(ctx);
 					if (avcodec_open2(ctx->c, codec, NULL) == 0)
 						using_hw = 1;
