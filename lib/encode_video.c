dd085ce64ba1cb5e6026a63d605c874ea4d283ff
franknag <49418361+franknag@users.noreply.github.com>
2025-01-14 11:19:02 -0500

    Update encode_video.c

--- lib/encode_video.c
+++ lib/encode_video.c
@@ -698,9 +698,9 @@ void open_video(VideoContext* ctx, Error* err)
 				{
 					ctx->c->pix_fmt = AV_PIX_FMT_YUV420P;
 					ctx->c->color_range = AVCOL_RANGE_MPEG;
+					ctx->c->profile = AV_PROFILE_H264_CONSTRAINED_BASELINE;
 					av_opt_set(ctx->c->priv_data, "realtime", "true", 0);
 					av_opt_set(ctx->c->priv_data, "allow_sw", "true", 0);
-					av_opt_set(ctx->c->priv_data, "profile", "baseline", 0);
 					av_opt_set(ctx->c->priv_data, "prio_speed", "true", 0);
 					set_codec_params(ctx);
 					if (avcodec_open2(ctx->c, codec, NULL) == 0)
