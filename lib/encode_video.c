83701d2cc6d9ddeff374544e6d7f36a0711d86ea
franknag <49418361+franknag@users.noreply.github.com>
2025-01-15 16:41:29 -0500

    Update encode_video.c

--- lib/encode_video.c
+++ lib/encode_video.c
@@ -715,7 +715,7 @@ void open_video(VideoContext* ctx, Error* err)
 					av_opt_set(ctx->c->priv_data, "crf", "38", 0);
 					set_codec_params(ctx);
 					if (avcodec_open2(ctx->c, codec, NULL) == 0)
-						using_hw = 0;
+						using_hw = 1;
 					else
 					{
 						log_debug("Could not open codec: %s!", av_err2str(ret));
